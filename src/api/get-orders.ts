1707a71eb9725b36768f112b1f0c93f995d6b782
jeanjacintho <jeanjacintho@yahoo.com.br>
2024-06-15 11:00:37 -0300

    order pagination

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,8 @@
 import { api } from '@/lib/axios'
 
+export interface GetOrdersQuery {
+  pageIndex?: number | null
+}
 interface GetOrdersResponse {
   orders: {
     orderId: string
@@ -14,10 +17,10 @@ interface GetOrdersResponse {
     totalCount: number
   }
 }
-export async function getOrders() {
+export async function getOrders({ pageIndex }: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   })
 
